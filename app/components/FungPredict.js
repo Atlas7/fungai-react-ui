87761f5bad7a1bcf8c632420b458f12aa73eaaa7
Atlas7 <johnnychan0302@gmail.com>
2017-11-17 14:58:47 +0000

    pretty ui with modal info box

--- app/components/FungPredict.js
+++ app/components/FungPredict.js
@@ -1,7 +1,94 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button, Modal, OverlayTrigger, Popover, Tooltip} from 'react-bootstrap'
 import { wait, fetchPredictions } from '../utils/fakeAPI'
 import Loading from './Loading'
+
+// utility function
+function zipPredClassesScores (pred) {
+  const trueClass  = pred.image.class
+  const { predictClasses, predictScores } = pred
+  const predObjects = predictClasses.map((e, i) => ({
+    predClass: e,
+    predScore: predictScores[i],
+    trueClass: trueClass,
+    predCorrect: e.wnid === trueClass.wnid
+  }))
+  return predObjects
+}
+
+
+class PredItemModal extends React.Component {
+  state = {
+    showModal: false
+  }
+  close = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+  open = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+  render = () => {
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    )
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    )
+    const { pred } = this.props
+    const { imageId, image, predictClasses, predictScores } = this.props.pred
+    const { imageURL } = image
+    return (
+      <div>
+        <Button
+          bsStyle="primary"
+          bsSize="small"
+          onClick={this.open}
+        >
+          more info
+        </Button>
+
+        <Modal show={this.state.showModal} onHide={this.close} bsSize="large">
+          <Modal.Header closeButton>
+            <Modal.Title>Image Classification Summary</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="row">
+              <div className="column">
+                <h4>Test Image</h4>
+                <img className='fung-photo-cover-2' src={imageURL} alt={':-('} />
+                <p><a href={imageURL} target="_blank">Image Source</a></p>
+              </div>
+              <div className="column">
+                <h4>Ground Truth</h4>
+                <TruthTable pred={pred} />
+              </div>
+            </div>
+
+            <hr />
+
+            <h4>Predictions</h4>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget
+              quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <PredictTables pred={pred}/>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+}
+
 
 
 function PredJSON ({pred}) {
@@ -11,7 +98,7 @@ function PredJSON ({pred}) {
     </div>
   )
 }
-TruthBox.PropTypes = {
+PredJSON.PropTypes = {
   pred: PropTypes.object.isRequired,
 }
 
@@ -23,40 +110,117 @@ function ItemLabel ({index}) {
     </div>
   )
 }
-TruthBox.PropTypes = {
+ItemLabel.PropTypes = {
   index: PropTypes.number.isRequired,
 }
 
+function FungPhoto ({imageURL}) {
+  return (
+    <img
+      className='fung-photo-cover'
+      src={imageURL}
+      alt={':-('}
+    />
+  )
+}
 
-function TruthBox ({pred}) {
-  const {image} = pred
+
+function PredictTables({pred}) {
+  const predObjects = zipPredClassesScores(pred)
+  return (
+    <ul>
+      {predObjects.map(({predScore, predClass, predCorrect}, index)=> (
+        <li key={index}>
+          <div>
+            <h5>{`Prediction #${index+1} / Score: ${predScore}`}</h5>
+            <table
+              className='predict-table'
+              style = {
+                predCorrect ? {
+                  "borderStyle": "solid",
+                  "borderWidth": "10px",
+                  "borderColor": "green"
+                } : {
+                  "borderStyle": "solid",
+                  "borderWidth": "10px",
+                  "borderColor": "red"
+                }
+              }
+            >
+              <tbody>
+                <tr>
+                  <th>{`Common Name`}</th>
+                  <td>{predClass.commonName}</td>
+                </tr>
+                <tr>
+                  <th>{`Latin Name`}</th>
+                  <td>{predClass.latinName}</td>
+                </tr>
+                <tr>
+                  <th>{`ImageNet Name`}</th>
+                  <td>{predClass.imagenetName}</td>
+                </tr>
+                <tr>
+                  <th>{`Imagenet Description`}</th>
+                  <td>{predClass.imagenetDescription}</td>
+                </tr>
+                <tr>
+                  <th>{`WordNet ID (wnid)`}</th>
+                  <td>{predClass.wnid}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+PredictTables.PropTypes = {
+  pred: PropTypes.object.isRequired,
+}
+
+
+function TruthTable ({pred}) {
+  const { image } = pred
+  const { imageURL } = image
+  const { wnid, imagenetName, commonName, latinName, imagenetDescription } = image.class
   return (
     <div className="truth-box">
-      <ul className='space-list-items'>
-        <img
-          className='fung-photo'
-          src={image.imageURL}
-          alt={':-('}
-        />
-        <li><a href={image.imageURL}>source</a></li>
-      </ul>
+      <table>
+        <tbody>
+          <tr>
+            <th>{`Common Name`}</th>
+            <td>{commonName}</td>
+          </tr>
+          <tr>
+            <th>{`Latin Name`}</th>
+            <td>{latinName}</td>
+          </tr>
+          <tr>
+            <th>{`ImageNet Name`}</th>
+            <td>{imagenetName}</td>
+          </tr>
+          <tr>
+            <th>{`Imagenet Description`}</th>
+            <td>{imagenetDescription}</td>
+          </tr>
+          <tr>
+            <th>{`WordNet ID (wnid)`}</th>
+            <td>{wnid}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
-TruthBox.PropTypes = {
+TruthTable.PropTypes = {
   pred: PropTypes.object.isRequired,
 }
 
 
 function PredBox ({pred}) {
-  const trueClass  = pred.image.class
-  const { predictClasses, predictScores } = pred
-  const predObjects = predictClasses.map((e, i) => ({
-    predClass: e,
-    predScore: predictScores[i],
-    trueClass: trueClass,
-    predCorrect: e.wnid === trueClass.wnid
-  }))
+  const predObjects = zipPredClassesScores(pred)
   return (
     <ul className="pred-box">
         {
@@ -84,6 +248,20 @@ function PredBox ({pred}) {
 }
 
 
+function PredItem({pred}) {
+  return (
+    <div>
+      <FungPhoto imageURL={pred.image.imageURL} />
+      <PredItemModal pred={pred} />
+      <PredBox pred={pred}  />
+    </div>
+  )
+}
+PredsGrid.PropTypes = {
+  pred: PropTypes.object.isRequired,
+}
+
+
 function PredsGrid ({preds}) {
   return (
     <div className='preds-grid'>
@@ -91,8 +269,7 @@ function PredsGrid ({preds}) {
         {preds.map((pred, index) => (
           <li key={pred.imageId} className='pred-item'>
             <ItemLabel index={index} />
-            <TruthBox pred={pred} />
-            <PredBox pred={pred}  />
+            <PredItem pred={pred} />
           </li>
         ))}
       </ul>
