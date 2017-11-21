b893ac851fc7d3a5f47f74f1ba657831c14114bc
Atlas7 <johnnychan0302@gmail.com>
2017-11-21 20:09:08 +0000

    update again

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,9 @@
-const webpack = require('webpack')
 const merge = require('webpack-merge')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
+    new UglifyJSPlugin()
   ]
 })
