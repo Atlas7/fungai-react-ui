f72b8fefa3362f38ef6c6d30865e49bb710e9f9f
Atlas7 <johnnychan0302@gmail.com>
2017-11-21 23:36:44 +0000

    use catch all to relieve broken route upon refresh on heroku

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get('/', function(request, response) {
+// Note: use `/*` (catch all) instead of just `/` to temporarily relieve broken link upon refreshing a route on heroku
+// https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writting-manually#answer-45597445
+app.get('/*', function(request, response) {
   // response.sendFile(__dirname + '/dist/index.html');
   response.sendFile(path.resolve(__dirname + '/dist/index.html'))
-});
+})
 
 app.listen(PORT, error => (
   error
     ? console.error(error)
     : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
-));
+))
