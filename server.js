00a16aeb148a3827827371be9c91e79968126609
Atlas7 <johnnychan0302@gmail.com>
2017-11-21 23:24:00 +0000

    attempt to fix broken route upon refresh

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/dist/index.html');
+  // response.sendFile(__dirname + '/dist/index.html');
+  response.sendFile(path.resolve(__dirname + '/dist/index.html'))
 });
 
 app.listen(PORT, error => (
